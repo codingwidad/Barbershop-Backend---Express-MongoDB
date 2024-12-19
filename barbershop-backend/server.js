f7fba7586a462adaf66f844f3db926c6f3dc5da7
codingwidad <brafik.widad@gmail.com>
2024-12-19 16:09:14 +0000

    Add URL value render

--- barbershop-backend/server.js
+++ barbershop-backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://barbershop-frontend-fii0.onrender.com',
 }));
 app.use(bodyParser.json());
 app.use(cors());
